20ca98022d64629fd64c08f6625af36597d85288
Luke Nimtz <luke.nimtz@gmail.com>
2016-01-01 12:31:15 -0800

    init

---
+++ Assets/BubbleBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class BubbleBehavior : MonoBehaviour {
+	public bool selected;
+	public Material selectedMaterial;
+
+	private Renderer rend;
+	private 
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("hello");
+		rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Events
+	void OnMouseDown () {
+		Debug.Log("down");
+		Select();
+	}
+
+	void OnMouseDrag () {
+		Debug.Log("drag");
+	}
+
+	void OnMouseExit () {
+		Debug.Log("exit");
+	}
+
+	void Select () {
+		selected = true;
+		rend.sharedMaterial = selectedMaterial;
+	}
+
+}
